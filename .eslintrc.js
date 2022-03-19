2181b83258d93dd90afd647197276faae8572ced
PeterPain <5794325+YSchroe@users.noreply.github.com>
2022-03-19 09:40:06 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        node: true,
+        'vue/setup-compiler-macros': true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        '@vue/eslint-config-typescript/recommended',
+        '@vue/eslint-config-prettier'
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+    }
+}
